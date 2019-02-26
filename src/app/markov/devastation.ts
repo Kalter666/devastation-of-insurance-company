c46348e1fbce5f055a1cc09a4ba3c1bc6e66bcff
Kalter <kalterxad34@gmail.com>
2019-02-26 21:10:30 +0300

    probability

--- src/app/markov/devastation.ts
+++ src/app/markov/devastation.ts
@@ -1,3 +1,6 @@
+import { exp } from 'mathjs';
+import * as mathjs from 'mathjs';
+
 interface CapitalRange {
   min: number;
   max: number;
@@ -45,5 +48,58 @@ class Devastation {
       capitals.push(i / 10);
     }
     return capitals;
+  }
+
+  private findExp(s: number, a: number, r: number, sigma: number): number {
+    return exp((s - a * r) / ((1 + sigma) * a));
+  }
+
+  private findUnderSum(
+    r: number,
+    a: number,
+    sigma: number,
+    s: number,
+    n: number
+  ): number {
+    const scope = { r, a, sigma, s, n };
+    const func = `((-1) ^ n) * (s - a * r) ^ n /
+     ((1 + sigma) ^ n) * (a ^ n) * r!`;
+    return mathjs.eval(func, scope);
+  }
+
+  private findPartOfSum(
+    s: number,
+    a: number,
+    r: number,
+    sigma: number,
+    n: number
+  ): number {
+    return this.findExp(s, a, r, sigma) * this.findUnderSum(r, a, sigma, s, n);
+  }
+
+  private findSum(
+    s: number,
+    a: number,
+    r: number,
+    sigma: number,
+    k: number
+  ): number {
+    let sum = 0;
+    for (let i = 0; i <= k; i++) {
+      sum += this.findPartOfSum(s, a, r, sigma, i);
+    }
+    return sum;
+  }
+
+  private findProbability(
+    s: number,
+    a: number,
+    r: number,
+    sigma: number,
+    k: number
+  ): number {
+    const left = sigma / (1 + sigma);
+    const sum = this.findSum(s, a, r, sigma, k);
+    return 1 - left * sum;
   }
 }
