491bf353f8262ffcb9e70395271cf561da75d70d
Iakov Kosiakov <kalterxad34@gmail.com>
2019-04-02 16:14:57 +0300

    upd with vector

--- src/app/markov/markov-chain.ts
+++ src/app/markov/markov-chain.ts
@@ -1,35 +1,20 @@
-import { matrix, Matrix, multiply } from 'mathjs';
+import { matrix, Matrix, multiply, transpose, MathArray, pow } from 'mathjs';
 
 export class MarkovChain {
-  private probs: Matrix[];
+  private init: Matrix;
+  private u: Matrix | MathArray;
 
-  constructor(p: number[][]) {
-    const probability = matrix(p);
-    this.probs.push(probability);
-  }
-
-  private findNextProb(probs: any): any {
-    const first = probs[0];
-    const last = probs[probs.length - 1];
-    return multiply(first, last);
+  constructor(p: number[][], u: number[]) {
+    this.init = matrix(p);
+    this.u = transpose(u);
   }
 
   private findProb(i: number): Matrix {
-    if (this.probs.length === 0 || i < 0) {
-      return null;
-    }
-    if (i < this.probs.length) {
-      return this.probs[i];
-    }
-    while (i >= this.probs.length) {
-      const nextProb = this.findNextProb(this.probs);
-      this.probs.push(nextProb);
-    }
-    return this.probs[i];
+    return; // TODO: implement this method
   }
 
   get initial(): Matrix {
-    return this.probs[0];
+    return this.init;
   }
 
   getProb(i: number): Matrix {
