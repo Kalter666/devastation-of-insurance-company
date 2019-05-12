8426c18fc8745859adf97649343d8ed0e209c2a5
iakov <kalterxad34@gmail.com>
2019-05-12 18:23:53 +0300

    removed garbage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,24 +6,16 @@ import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ChartComponent } from './chart/chart.component';
 import { HeaderComponent } from './main/header/header.component';
 import { Model2Component } from './model2/model2.component';
 import { GraphComponent } from './shared/graph/graph.component';
-import { MarkovChainComponent } from './markov-chain/markov-chain.component';
-import { TableComponent } from './markov-chain/table/table.component';
-import { InputFormComponent } from './markov-chain/input-form/input-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChartComponent,
     GraphComponent,
     Model2Component,
     HeaderComponent,
-    MarkovChainComponent,
-    TableComponent,
-    InputFormComponent
   ],
   imports: [
     BrowserModule,
@@ -36,4 +28,4 @@ import { InputFormComponent } from './markov-chain/input-form/input-form.compone
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
