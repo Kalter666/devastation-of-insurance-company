d1ac0ab21c452c85b27078a33335cec41033c399
iakov <kalterxad34@gmail.com>
2019-05-12 18:48:16 +0300

    model 1 service

--- src/app/services/model1.service.ts
+++ src/app/services/model1.service.ts
@@ -1,9 +1,68 @@
 import { Injectable } from '@angular/core';
 
+import { eval as mathEval } from 'mathjs';
+
+export interface CapitalRange {
+  min: number;
+  max: number;
+}
+
+export const defaultEpsilon = 1e-1;
+
 @Injectable({
   providedIn: 'root'
 })
 export class Model1Service {
+  S: number[];
+  a: number;
+  b: number;
+  theta: number;
+  P: number[];
 
-  constructor() { }
+  constructor(
+    capitalRange: CapitalRange,
+    a: number,
+    b: number,
+    theta: number,
+    epsilon: number = defaultEpsilon
+  ) {
+    this.a = a;
+    this.b = b;
+    this.theta = theta;
+    this.buildCapitals(capitalRange, epsilon);
+  }
+
+  private buildCapitals(capitalRange: CapitalRange, epsilon: number) {
+    const S: number[] = [];
+    for (let i = capitalRange.min; i < capitalRange.max; i += epsilon) {
+      S.push(i);
+    }
+    this.S = S;
+  }
+
+  private buildProbs() {
+    const formula = '(a + b) / (a + b * (1 + theta)) * e ^ (-(theta * S)/(a + b * (1 + theta)))';
+    const baseScope = {
+      a: this.a,
+      b: this.b,
+      theta: this.theta
+    };
+    const capitals = this.S;
+    const P: number[] = [];
+    for (const S of capitals) {
+      const scope = {...baseScope, S};
+      const res = mathEval(formula, scope);
+      P.push(res);
+    }
+    this.P = P;
+  }
+
+  get probs() {
+    this.buildProbs();
+    return this.P;
+  }
+
+  get caps() {
+    return this.S;
+  }
 }
