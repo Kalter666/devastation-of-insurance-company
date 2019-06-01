5b8aa114e5936f1306f39316f2b514b6fe2aca91
iakov <kalterxad34@gmail.com>
2019-06-01 15:01:06 +0300

    app translation module

---
+++ src/app/app-translate.module.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient);
+}
+
+@NgModule({
+  imports: [
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+  ],
+  exports: [TranslateModule]
+})
+export class AppTranslateModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 import { AppRoutingModule } from './app-routing.module';
+import { AppTranslateModule } from './app-translate.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './main/header/header.component';
 import { Model1Component } from './model1/model1.component';
@@ -24,12 +25,13 @@ import { HintComponent } from './shared/hint/hint.component';
     HintComponent,
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
-    NgxChartsModule,
+    AppTranslateModule,
     BrowserAnimationsModule,
+    BrowserModule,
+    NgxChartsModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
